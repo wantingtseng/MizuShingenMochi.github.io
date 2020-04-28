b22a09209d2eb590b6cd399f26e9ce5a10c2338b
wantingtseng <63231979+wantingtseng@users.noreply.github.com>
2020-04-28 17:33:57 +0800

    Add files via upload

---
+++ js/page2_script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.choose_flower').click(function () {
+         $('html,body').animate({scrollTop:$('#flower').offset().top},800);
+   
+});
+    $('.choose_color').click(function () {
+         $('html,body').animate({scrollTop:$('#color').offset().top},800);
+                            });
+    $('.choose_tea').click(function () {
+         $('html,body').animate({scrollTop:$('#tea').offset().top},800);
+                            });
+    });
